c1444c24c4a961006ec4f500304cd1eb2edac0fa
Anna Wästling <45712410+AnnaWastling@users.noreply.github.com>
2022-04-10 16:24:11 +0200

    scene

---
+++ js/Scene.js
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(200, 500, 300);
+scene.add(directionalLight);
+
+// Setting up camera
+const aspectRatio = window.innerWidth / window.innerHeight;
+const cameraWidth = 150;
+const cameraHeight = cameraWidth / aspectRatio;
+
+const camera = new THREE.OrthographicCamera(
+  cameraWidth / -2, // left
+  cameraWidth / 2, // right
+  cameraHeight / 2, // top
+  cameraHeight / -2, // bottom
+  0, // near plane
+  1000 // far plane
+);
+camera.position.set(200, 200, 200);
+camera.lookAt(0, 10, 0);
+
+// Set up renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+document.body.appendChild(renderer.domElement);
