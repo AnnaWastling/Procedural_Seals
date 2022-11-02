affb23f6726f7788fc353e59563866bd6beead3f
Anna Wästling <45712410+AnnaWastling@users.noreply.github.com>
2022-11-02 17:16:56 +0100

    almost there

--- World.js
+++ World.js
@@ -40,10 +40,15 @@ const assetLoader = new GLTFLoader();
 const objects = [];
 const mixers = [];
 assetLoader.load(modelUrl.href, function(gltf) {
+    // load a texture, set wrap mode to repeat
+    var texture = new THREE.TextureLoader().load( "sealUV.png" );
+    // texture.wrapT = THREE.RepeatWrapping;
+    // texture.wrapS = THREE.RepeatWrapping;
+    // texture.repeat.set(2,2);
     const model = gltf.scene;
-    for (let index = 0; index < 20; index++) {
+    for (let index = 0; index < 100; index++) {
         const sealClone = SkeletonUtils.clone(model);
-        sealClone.position.set(Math.random()*-300, 10, Math.random()*-300);
+        sealClone.position.set(Math.random()*-150, 10, Math.random()*-150);
         // var testLineMaterial = new THREE.LineBasicMaterial({ color: 0xFF0000 });
         // var points = [];
         // points.push(new THREE.Vector3(sealClone.position.x, sealClone.position.y + 10, sealClone.position.z));
@@ -82,20 +87,18 @@ assetLoader.load(modelUrl.href, function(gltf) {
                 break;
             }else{
                 objects[i].traverse( ( object ) => {
-                    const color = new THREE.Color();
-                    const palette = [ 0x736749, 0x735b49, 0xcfb5a1, 0x4a433e, 0x4a4948 ];
-                    color.setHex( palette[ Math.floor( Math.random() * palette.length ) ] );
-                    if ( object.name == 'Cube001_1' && object.geometry ) {
-                        const material1 = new THREE.MeshLambertMaterial({ color: color });
-                        const material2 = new THREE.MeshLambertMaterial({ color: color });
-                        let materials = [material1, material2];
-                        let geometry = object.geometry;
-                        geometry.addGroup(0, Infinity, 0);
-                        geometry.addGroup(0, Infinity, 1);
-
-                        object.material = materials;
+                    if ( object.name == 'Cube001_1' && object.geometry) {
+                        //let tex = generateTexture();
+                        const color = new THREE.Color();      
+                        const palette = [ 0xcfb5a1, 0x4a433e, 0x4a4948 ];
+                        color.setHex(palette[Math.floor( Math.random() * palette.length )]);
+                        // const material = new THREE.MeshLambertMaterial({ 
+                        //     color:color,
+                        //     map: texture,
+                        // });
+                        //object.material = material;
+                        object.material.color = color;
                     }
-
                 } );
                 //console.log(objects[i]);
                 scene.add(objects[i]);
@@ -132,61 +135,92 @@ function MakeTerrain()
     const normals = [];
     const colors = [];
 
-    const size = 500;
+
+    const size = 300;
     const segments = 50;
 
     const halfSize = size / 2;
     const segmentSize = size / segments;
 
     // generate vertices, normals and color data for a simple grid geometry
-    for ( let i = 0; i <= segments; i ++ ) {
-        for ( let j = 0; j <= segments; j ++ ) {
+    for (let i = 0; i <= segments; i ++) {
+        for (let j = 0; j <= segments; j ++) {
             const perlin = new ImprovedNoise();
             const noise = new SimplexNoise();
-            const z = ( i * segmentSize ) - halfSize
-            const x = ( j * segmentSize ) - halfSize;
+            const z = (i * segmentSize) - halfSize
+            const x = (j * segmentSize) - halfSize;
             const y = Math.abs(perlin.noise(x/halfSize, 1, z/halfSize) * segmentSize* 5 + noise.noise(x/size, 1, z/size) * segmentSize*0.2 );
-            vertices.push( x, y, z);
-            normals.push( 0, 1, 0 );
+            vertices.push(x, y, z);
+            normals.push(0, 1, 0);
 
-            const r = ( y / size)* 10;
-            const g = ( y / size)* 10;
-            const b = ( y / size)* 10;
-            colors.push( r*2, g*1.5, b );
-
+            const r = (y / size)* 10;
+            const g = (y / size)* 10;
+            const b = (y / size)* 10;
+            colors.push(r*2, g*1.5, b);
         }
-
     }
 
     // generate indices (data for element array buffer)
-    for ( let i = 0; i < segments; i ++ ) {
+    for (let i = 0; i < segments; i ++) {
+        for (let j = 0; j < segments; j ++) {
 
-        for ( let j = 0; j < segments; j ++ ) {
-
-            const a = i * ( segments + 1 ) + ( j + 1 );
-            const b = i * ( segments + 1 ) + j;
-            const c = ( i + 1 ) * ( segments + 1 ) + j;
-            const d = ( i + 1 ) * ( segments + 1 ) + ( j + 1 );
+            const a = i * (segments + 1) + (j + 1);
+            const b = i * (segments + 1) + j;
+            const c = (i + 1) * (segments + 1) + j;
+            const d = (i + 1) * (segments + 1) + (j + 1);
 
             // generate two faces (triangles) per iteration
-            indices.push( a, b, d ); // face one
-            indices.push( b, c, d ); // face two
+            indices.push(a, b, d); // face one
+            indices.push(b, c, d); // face two
 
         }
     }
 
-    geometry.setIndex( indices );
-    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-    geometry.setAttribute( 'normal', new THREE.Float32BufferAttribute( normals, 3 ) );
-    geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+    geometry.setIndex(indices);
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    geometry.setAttribute('normal', new THREE.Float32BufferAttribute(normals, 3));
+    geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
 
-    const material = new THREE.MeshPhongMaterial( {
+    const material = new THREE.MeshPhongMaterial({
         side: THREE.DoubleSide,
         vertexColors: true
-    } );
+    });
 
-    const mesh = new THREE.Mesh( geometry, material );
-    mesh.position.set(-150,0,-150);
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(-50,0,-50);
     scene.add(mesh);
     return mesh;
 }
+
+// function generateTexture(){
+//     const color = new THREE.Color();      
+//     const palette = [ 0x736749, 0x735b49, 0xcfb5a1, 0x4a433e, 0x4a4948 ];
+//     color.setHex( palette[ Math.floor( Math.random() * palette.length ) ] );
+//     const width = 256;
+//     const height = 256;
+//     const size = width * height;
+//     const data = new Uint8Array( 4 * size );
+//     const perlin = new SimplexNoise();
+
+    
+//     for (let i = 0; i < size; i++) {
+        
+//         const r = Math.floor( color.r * 255);
+//         const g = Math.floor( color.g * 255);
+//         const b = Math.floor( color.b * 255);
+//         //console.log(r);
+//         const stride = i * 4;
+//         const scale = 50;
+//         data[ stride ] = r * Math.abs(perlin.noise(i/width * scale, i/width* scale, i/width* scale));
+//         data[ stride + 1 ] = g * Math.abs(perlin.noise(i/width* scale, i/width* scale, i/width* scale));
+//         data[ stride + 2 ] = b * Math.abs(perlin.noise(i/width* scale, i/width* scale, i/width* scale));
+//         data[ stride + 3 ] = 255;
+    
+//     }
+    
+//     // used the buffer to create a DataTexture
+//     const texture = new THREE.DataTexture( data, width, height );
+//     texture.needsUpdate = true;
+//     return texture;
+// }
+
